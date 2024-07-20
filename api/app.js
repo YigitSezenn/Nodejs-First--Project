e88250317863f42346cdc4269139c1fcf39bb9fb
Yigit Sezen <yigit.sezen@outlook.com>
2024-07-20 14:08:31 +0300

    update

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var auditlogs = require('./routes/auditlogs');
 
 var app = express();
 
@@ -36,7 +37,7 @@ next();
 
 app.use('/', indexRouter); //http://localhost:3000/
 app.use('/users', usersRouter); //http://localhost:3000/users
-app.use("/auditlogs",auditlogs); //http://localhost:3000/auditlogs
+app.use('/auditlogs',auditlogs); 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
